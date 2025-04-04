e32d5971cf5c11e6d3fe175e57824533e174be9d
sunwoowoo <po8702@naver.com>
2025-04-04 09:46:18 +0900

    123
    123

---
+++ project03/src/main/java/com/project03/repository/PlaceRepository.java
@@ -0,0 +1,10 @@
+package com.project03.repository;
+
+import com.project03.entity.Place;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PlaceRepository extends JpaRepository<Place, Long> {
+    Optional<Place> findByPlaceNameAndAddress(String placeName, String address);
+}
