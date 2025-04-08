d9561578c732a557dd1f6705b8183c168eb79a3d
sunwoowoo <po8702@naver.com>
2025-04-08 11:03:37 +0900

    삭제
    삭제

--- project03/src/main/java/com/project03/repository/manufacturing/MaterialRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project03.repository.manufacturing;
-
-import com.project03.domain.Material;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MaterialRepository extends JpaRepository<Material, Long> {
-    // 필요 시 커스텀 메서드 작성 가능
-}
